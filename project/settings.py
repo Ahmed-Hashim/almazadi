b7348a18de5b3337eb6fed7eee302f177fea9f9e
Ahmed-Hashim <ah.abolaban@gmail.com>
2022-10-20 14:49:29 +0200

    a

--- project/settings.py
+++ project/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -188,7 +192,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ah.abolaban@gmail.com'
-EMAIL_HOST_PASSWORD ='iomsaybasdlhatnr'
+EMAIL_HOST_USER = env('EMAIL_USER')
+EMAIL_HOST_PASSWORD =env('EMAIL_PASS')
 
 
